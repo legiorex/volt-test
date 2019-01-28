ce8c1b54f615ac2583e0a5cf9fddc23396000018
legiorex <batyushev@gmail.com>
2019-01-29 00:36:15 +1000

    init commit

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import {render} from "react-dom";
+
+import 'react-select/dist/react-select.css';
+
+
+
+render(<div> Place your application here</div>, document.getElementById("app-root"));
